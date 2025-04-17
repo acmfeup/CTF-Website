550eee0254b0781696d2aae5c21b608dc066e528
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-04-17 22:31:52 +0100

    feat: button now has hover effect

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -7,8 +7,8 @@ const Jersey = Jersey_10({
 
 export default function RegisterButton() {
   return (
-    <a className="rounded-full border border-solid border-main-green/40 bg-transparent backdrop-blur-sm px-10 py-5 animate-glow cursor-pointer">
-      <p className={`text-6xl ${Jersey.className}`}>Register you team</p>
+    <a className="rounded-full border border-solid border-main-green/40 bg-transparent backdrop-blur-sm px-10 py-5 animate-glow cursor-pointer hover:bg-main-green/40 hover:text-white hover-fill">
+      <p className={`text-6xl ${Jersey.className}`}>Register your team</p>
     </a>
   );
 }
