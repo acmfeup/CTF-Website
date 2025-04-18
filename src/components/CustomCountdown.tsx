860d7818a5428ae343c0b22d7a65ca259a204b62
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-04-18 14:59:00 +0100

    feat: countdown responsiveness

--- src/components/CustomCountdown.tsx
+++ src/components/CustomCountdown.tsx
@@ -14,17 +14,21 @@ export default function CustomCountdown() {
     seconds: number;
   }
 
+  function Separator() {
+    return <div className="hidden lg:block w-0.5 bg-gray-200/10"></div>;
+  }
+
   const renderer = ({ days, hours, minutes, seconds }: CountdownProps) => {
     return (
       <div
-        className={`flex flex-row items-stretch justify-center gap-16 bg-secondary-green/20 rounded-4xl px-16 ${jost.className}`}
+        className={`grid grid-cols-2 md:grid-cols-2 lg:flex lg:flex-row items-stretch justify-center gap-y-8 gap-x-16 bg-secondary-green/20 rounded-4xl px-16 ${jost.className}`}
       >
         <ValueContainer value={days} label="Days" />
-        <div className="w-0.5 bg-gray-200/10"></div>
+        <Separator />
         <ValueContainer value={hours} label="Hours" />
-        <div className="w-0.5 bg-gray-200/10"></div>
+        <Separator />
         <ValueContainer value={minutes} label="Minutes" />
-        <div className="w-0.5 bg-gray-200/10"></div>
+        <Separator />
         <ValueContainer value={seconds} label="Seconds" />
       </div>
     );
@@ -46,7 +50,7 @@ interface ValueContainerProps {
 
 function ValueContainer({ value, label }: ValueContainerProps) {
   return (
-    <div className="flex flex-col py-8">
+    <div className="flex flex-col py-8 text-center items-center justify-center">
       <p className="font-bold text-6xl">{value}</p>
       <p className="text-white/50 text-2xl">{label}</p>
     </div>
