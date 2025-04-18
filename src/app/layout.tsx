6afb7b0e07f25922286b3b1718e1aa0e2b1d99f1
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-04-18 01:28:57 +0100

    fix: renamed mouse follower blob

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopNavBar from "@/components/TopNavBar";
-import MouseFollowBlob from "@/components/MouseFollowerBlob";
+import MouseFollowerBlob from "@/components/MouseFollowerBlob";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MouseFollowBlob />
+        <MouseFollowerBlob />
         <TopNavBar />
         {children}
       </body>
