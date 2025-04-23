1aff97bb48efdfbcbd5323509a66b145fd031473
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-04-23 19:34:50 +0100

    fix: lint\

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -12,6 +12,7 @@ export default function RegisterButton() {
       target="_blank"
       rel="noopener noreferrer"
       className="rounded-full border border-solid border-main-green/40 bg-transparent backdrop-blur-sm px-10 py-5 animate-glow cursor-pointer hover:bg-main-green/40 hover:text-white hover-fill"
+    >
       <p className={`text-4xl sm:text-6xl ${Jersey.className}`}>
         Register your team
       </p>
