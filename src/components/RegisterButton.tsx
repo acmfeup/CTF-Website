89521984925096c8f0c1d60f508f5eee229e8bbd
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2025-05-17 00:31:41 +0100

    feat: closed registrations

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -7,15 +7,24 @@ const Jersey = Jersey_10({
 
 export default function RegisterButton() {
   return (
-    <a
-      href="https://forms.gle/ATWNsKV1Up6qQJkJ7"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="rounded-full border border-solid border-main-green/40 bg-transparent backdrop-blur-sm px-10 py-5 animate-glow cursor-pointer hover:bg-main-green/40 hover:text-white hover-fill"
-    >
+    <div className="rounded-full border border-solid border-main-green/40 bg-transparent backdrop-blur-sm px-10 py-5">
       <p className={`text-4xl sm:text-6xl ${Jersey.className}`}>
-        Register your team
+        Registration is Closed
       </p>
-    </a>
+    </div>
   );
+  {
+    /*
+		<a
+			href="https://forms.gle/ATWNsKV1Up6qQJkJ7"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="rounded-full border border-solid border-main-green/40 bg-transparent backdrop-blur-sm px-10 py-5 animate-glow cursor-pointer hover:bg-main-green/40 hover:text-white hover-fill"
+		>
+				<p className={`text-4xl sm:text-6xl ${Jersey.className}`}>
+					Register your team
+				</p>
+		</a>
+			*/
+  }
 }
